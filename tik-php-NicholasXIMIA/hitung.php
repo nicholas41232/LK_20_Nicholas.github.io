ac55a51475c4d36038f0080a395b14bce8d9082b
nicholas41232 <110577678+nicholas41232@users.noreply.github.com>
2023-03-16 10:44:13 +0800

    Add files via upload

---
+++ tik-php-NicholasXIMIA/hitung.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+            $a = 10;
+            $b = 10;
+            $c = $a + $b;
+            echo"$c <br>";
+        ?> 
+    <a href = "menu.php">Kembali ke menu utama </a>
+</body>
+</html>
---
+++ tik-php-NicholasXIMIA/menu.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+<?php
+    $nama = "Nicholas Ghonius";
+    echo "Kelas 11 IPA<br>";// menampilkan tulisan
+    echo "Nama : $nama<br>";
+?>
+<a href="hitung.php">Menuju Page Perhitungan</a>
+</body>
+</html>
